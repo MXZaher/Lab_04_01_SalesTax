c2ab84981729b9298aef2f19051ea318984b25a5
maxwe <maxwell.zaher@gmail.com>
2024-09-29 17:27:59 -0400

    init commit

---
+++ src/salesTax.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class salesTax {
+    public static void main(String[] args) {
+        System.out.print("Enter the price: ");
+        Scanner sc = new Scanner(System.in);
+        double price = sc.nextInt();
+        double tax = (.05 * price);
+        price = (double) (price + tax);
+        System.out.println("Your total after tax is: " + price);
+    }
+}
